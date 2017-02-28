7a6444bce1d5fddd938126353a63707439653c03
Cody <c.w.williams00@gmail.com>
2017-02-28 16:01:55 -0600

    --Added functionality

--- algebra.py
+++ algebra.py
@@ -17,6 +17,13 @@ def main():
 	unknown = random.randint (0, 20)
 	answer = (x * unknown) + y
 	
-	print (str(x) * 'X' + str(y) '=' str(answer))
+	print (str(x) + " * " + ' X ' + " + " + str(y) + " = " + str(answer))
+	
+	answer_x = input("What is X? ")
+	
+	if int(answer_x) == unknown:
+	  print ("Good job!")
+	else:
+		print ("Sorry, the answer is " + str(unknown))
 	
 main()
