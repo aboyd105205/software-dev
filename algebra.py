ef20503346ed77ec165150fcf5cc8af1813ba336
Cody <c.w.williams00@gmail.com>
2017-02-28 16:16:26 -0600

    --

--- algebra.py
+++ algebra.py
@@ -5,16 +5,19 @@ def main():
 	numTwo = random.randint (0, 1)
 	
 	if numOne == 0:
-		x = random.randint (0, 20)
+		x = random.randint (1, 20)
 	else:
-		x = random.randint (-20, 0)
+		x = random.randint (-20, -1)
 
 	if numTwo == 0:
-		y = random.randint (0, 20)
+		y = random.randint (1, 20)
 	else:
-		y = random.randint (-20, 0)
+		y = random.randint (-20, -1)
 		
-	unknown = random.randint (0, 20)
+	unknown = random.randint (-20, 20)
+	if unknown == 0:
+	  unknown = unknown + 1
+	
 	answer = (x * unknown) + y
 	
 	print (str(x) + " * " + ' X ' + " + " + str(y) + " = " + str(answer))
