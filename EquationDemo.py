27220fe083bdd3fba2d0081adbbd8bb307c6ca8b
Ashton <aboyd52501@gmail.com>
2017-03-02 16:57:18 -0600

    Added basic linear equation objects and test demo

---
+++ EquationDemo.py
@@ -0,0 +1,6 @@
+import Equations as Eqs
+
+# solves for f(x) or Y
+func = Eqs.LinearEquationY( 10, 1, 15 )
+print func.toString()
+print func.solve()
---
+++ Equations.py
@@ -0,0 +1,47 @@
+from abc import ABCMeta, abstractmethod
+
+class Equation:
+	
+	__metaclass__ = ABCMeta
+	
+	# abstract method that must be overridden for a class to work
+	@abstractmethod
+	def solve(self):
+		pass
+	
+	@abstractmethod
+	def toString(self):
+		pass
+	
+
+# an equation of the type f(x) = mx + b
+# this version solves for x
+class LinearEquationX(Equation):
+	
+	def __init__( self, m, b, fx ):
+		self.slope = m
+		self.offset = b
+		self.known = fx
+	
+	def solve(self):
+		return ( self.known - self.offset ) / self.slope
+	
+	def toString(self):
+		return '{0} = {1}*x + {2}'.format( *(self.known, self.slope, self.offset) )
+
+# an equation of the type f(x) = mx + b
+# this version solves for f(x)
+class LinearEquationY(Equation):
+	
+	def __init__( self, m, b, x ):
+		self.slope = m
+		self.offset = b
+		self.known = x
+	
+	def solve(self):
+		return self.known * self.slope + self.offset
+	
+	def toString(self):
+		return 'f(x) = {0}*{1} + {2}'.format( *(self.slope, self.known, self.offset) )
+
+
