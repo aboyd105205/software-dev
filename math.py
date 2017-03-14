f9628e6e01b237a008ad0e1723f94b72d2b20f9d
Student <>
2017-03-14 15:55:38 -0500

    Added to math.py

--- math.py
+++ math.py
@@ -9,9 +9,20 @@ def getInput():
 def checkIfCorrect( answer, equation ):
 	return answer == equation.solve()
 	
+def chooseEquation
+	random.seed()
+	eqsChosen = random.randint (1, 4)
+	print (eqsChosen)
+	if eqsChosen == 1:
+		lineFunc()
+	else:
+	
 # f(x) = mx + b
-lineFunc = eqs.LinearEquationY ( 10, 1, 15 )
-print(lineFunc.toString())
+def lineFunc()
+	lineFunc = eqs.LinearEquationY ( 10, 1, 15 )
+	print(lineFunc.toString())
+
+
 
 # Check input against real answer
 userInput = getInput()
