b2b17e5e3396e4637f8541f9c8cbaf438c965556
Aboyd105205 <aboyd52501@gmail.com>
2017-03-14 15:44:50 -0500

    updated checkIfCorrect function

--- math.py
+++ math.py
@@ -1,14 +1,23 @@
 import random
-import Equations
+import Equations as Eqs
+import math
 
 def main():
 	
 	def checkIfCorrect( answer, equation ):
+		
+		solution = round( equation.solve(), 3 )
+		ans = round( answer, 3 )
 		correct = False
-		for i, k in equation.solve():
-			if k == answer:
-				correct = True
-				break
+		
+		if isinstance( solution, float ) or isinstance( solution, int ):
+			correct = ( solution == ans )
+		else if isinstance( solution, list ):
+			for k in equation.solve():
+				if round( k, 3 ) == ans:
+					correct = True
+					break
+		
 		return correct
 	
 	
