62b8338aa6e51c0a9cdb59862233afa0f147153b
Student <>
2017-03-15 16:28:51 -0500

    Fixed readability, added random number generation, and introduced error handling for string input

--- math.py
+++ math.py
@@ -4,6 +4,15 @@ import math
 
 def main():
 
+	# Section for equation functions
+	
+	# f(x) = mx + b
+	def lineFunc(num1, num2, num3):
+		lineFunc = eqs.LinearEquationY (num1, num2, num3)
+		print(lineFunc.toString())
+		return lineFunc
+
+	# Computing section
 	def checkIfCorrect( answer, equation ):
 		
 		solution = round( equation.solve(), 3 )
@@ -22,20 +31,22 @@ def main():
 
 	def getInput():
 		userInput = input('Please enter the correct answer for the unknown variable: ')
-		return float(userInput)
-		
-	# f(x) = mx + b
-	def lineFunc():
-		lineFunc = eqs.LinearEquationY ( 10, 1, 15 )
-		print(lineFunc.toString())
-		return lineFunc
+		while True:
+			try:
+				userInput = float(userInput)
+				break
+			except ValueError:
+				userInput = input ("Please input an actual number: ")
+		return userInput
 		
 	def chooseEquation():
 		random.seed()
 		eqsChosen = random.randint (1, 1)
-		print (eqsChosen)
+		randNum1 = random.randint (-20, 20)
+		randNum2 = random.randint (-20, 20)
+		randNum3 = random.randint (-20, 20)
 		if eqsChosen == 1:
-			chosenEq = lineFunc()
+			chosenEq = lineFunc(randNum1, randNum2, randNum3)
 			return chosenEq
 
 	chosenEq = chooseEquation()
@@ -43,10 +54,11 @@ def main():
 	# Check input against real answer
 	userInput = getInput()
 	check = checkIfCorrect(userInput, chosenEq)
+	correct = chosenEq.solve()
 
 	if check:
 		print ("Good job!")
 	else:
-		print ("Sorry, that's the wrong answer.")
+		print ("Sorry, that's the wrong answer. It was actually {0}.".format (correct))
 
 main()
